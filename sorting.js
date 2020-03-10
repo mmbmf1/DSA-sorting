8c827bda5c792309e85de660932dc9bcf092e71b
Michael <mmbmf.mm@gmail.com>
2020-03-10 10:09:25 -0500

    completed thru drill 1

--- sorting.js
+++ sorting.js
@@ -34,6 +34,20 @@ function merge(left, right, array) {
   return array;
 }
 
+// 1. Understanding merge sort
+// Given the following list of numbers 21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
+
+// What is the resulting list that will be sorted after 3 recursive calls to mergesort?
+// [21, 1, 26, 45, 29, 28, 2, 9] -- [16, 49, 39, 27, 43, 34, 46, 40]
+// [21, 1, 26, 45] -- [29, 28, 2, 9] ----- [16, 49, 39, 27] -- [43, 34, 46, 40]
+// [21, 1] -- [26, 45] --- [29, 28] -- [2, 9] ----- [16, 49] -- [43, 34] -- [46, 40]
+
+// What is the resulting list that will be sorted after 16 recursive calls to mergesort?
+
+// What are the first 2 lists to be merged?
+
+// Which two lists would be merged on the 7th merge?
+
 console.log(
   mergeSort([21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40])
 );
